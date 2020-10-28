667624922e68267fd66e4b78f0dfacd06e156d7d
lzy <lizhenyu0016@163.com>
2020-10-28 20:14:23 +0800

    wf we

--- hello.lua
+++ hello.lua
@@ -1,1 +1,50 @@
-print("hello")
+local l = 1
+local m = 2
+
+local s = (l == m);
+print(s);
+--PromotionDataMgr
+--BingoTailBar (促销时间，两个促销切换)
+--CommonBillingMediator (defaultSuccess,addPurchaseItemFromServer) 
+--ShopLayer (onExit)
+
+--------------------
+
+--AlbumOpenPack(开卡包）
+--AlbumGainSingle(获得单卡) 
+--只有以上两种方法可以获得新卡
+ 
+-- AlbumDataMgr.isOwnCardById(id) --判断是否有这张卡
+
+-- 数据 -》UI ——》动画
+
+
+--PromotionDataMgr
+        --BingoTailBar (促销时间，两个促销切换)
+        --CommonBillingMediator (defaultSuccess,addPurchaseItemFromServer)
+        --ShopLayer (onExit)
+        
+        --------------------
+        
+        --AlbumOpenPack(开卡包）
+        --AlbumGainSingle(获得单卡)
+
+
+-- "<var>" = {
+--     "item" = {
+--         1 = {
+--             "identifier" = "magic_2_sale_1"
+--             "powerlist" = *MAX NESTING*
+--             "price"      = "9.99"
+--             "ticket"     = 2600
+--         }
+--         2 = {
+--             "golden"     = 5
+--             "identifier" = "magic_2_sale_1_2"
+--             "price"      = "9.99"
+--             "ticket"     = 1900
+--         }
+--     }
+--     "lefttime" = 3054
+--    }
+    
